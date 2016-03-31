0fd36d265be3c1755d1d471d99d580131c3e125c
Kacheflowe <basel.farag@gmail.com>
2016-03-31 01:44:36 -0600

    Being RNN implementation

--- Source/RNN.swift
+++ Source/RNN.swift
@@ -9,3 +9,11 @@
 import Foundation
 import Accelerate
 
+
+/*The structure of an RNN is highly similar to that of 
+ an FFNN with differences -- RNN possesses the ability to send
+ signals backwards and forwards and have dynamic states*/
+
+public enum RNNError: {
+    
+}
